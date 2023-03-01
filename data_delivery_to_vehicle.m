15b7b32ea5d25388da76e049e6b7c8a8fd2c7eff
RazonJoseph <razonjoseph123@gmail.com>
2023-03-01 06:11:34 +0530

    Changed the optimization function to better accomadate priority for vehicles with more data requested

--- data_delivery_to_vehicle.m
+++ data_delivery_to_vehicle.m
@@ -5,18 +5,23 @@ nov = N;
 
 cvx_begin
     variable data_to_vehicle(nov);
-    expression leftover_data(nov);
-    expression leftover_ratio(nov);
-    expression leftover_ratio_sum;
+    expression ratio(nov);
+    expression ratio_sum;
+    %expression leftover_data(nov);
+    %expression leftover_ratio(nov);
+    %expression leftover_ratio_sum;
 
 
     for i = 1:nov
-        leftover_data(i) = mem_req(Vec_set(i))-data_to_vehicle(i);
-        leftover_ratio(i) = leftover_data(i)/(num_min_edges(Vec_set(i)));
-        leftover_ratio_sum = leftover_ratio_sum + leftover_ratio(i);
+        %leftover_data(i) = mem_req(Vec_set(i))-data_to_vehicle(i);
+        %leftover_ratio(i) = leftover_data(i)*(num_min_edges(Vec_set(i)));
+        %leftover_ratio_sum = leftover_ratio_sum + leftover_ratio(i);
+        ratio(i) = (mem_req(Vec_set(i)) - data_to_vehicle(i))/(mem_req(Vec_set(i))*num_min_edges(Vec_set(i)));
+        ratio_sum = ratio_sum + ratio(i);    
     end
 
-    minimize leftover_ratio_sum
+    minimize ratio_sum
+    %minimize leftover_ratio_sum
     % maximize sum(data_to_vehicle)
     subject to
 
@@ -24,15 +29,15 @@ cvx_begin
             data_to_vehicle(i) >= 0;
         end
 
-        for i=1:nov
-            leftover_data(i) >= 0;
-        end
+        %for i=1:nov
+            %leftover_data(i) >= 0;
+        %end
 
-        for i=1:nov
-            leftover_ratio(i) >= 0;
-        end
+        %for i=1:nov
+            %leftover_ratio(i) >= 0;
+        %end
 
-        leftover_ratio_sum >= 0;
+        %leftover_ratio_sum >= 0;
         
         for i=1:nov
             data_to_vehicle(i) <= mem_req(Vec_set(i));
